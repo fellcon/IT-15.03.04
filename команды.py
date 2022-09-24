f128c6fe949adba5b64f680505f9c5d4f97f8e96
fellcon <113799501+fellcon@users.noreply.github.com>
2022-09-25 01:55:30 +0300

    Add files via upload

---
+++ команды.py
@@ -0,0 +1,21 @@
+print(6**7)
+
+word = 'python'
+print(word.capitalize())
+
+thing = 'pen'
+place = 'bag'
+print('Can you put a {} in the {}?'.format(thing,place))
+
+quotes = {
+    "Den": "Can you give me a glass of water?",
+    "Jane": "Hello everybody!",
+    "Bird": "chik chirik",
+}
+stooge = "Jane"
+print(stooge, "says:", quotes[stooge])
+
+p = tuple('Python')
+print(p)
+print(len(p))
+
